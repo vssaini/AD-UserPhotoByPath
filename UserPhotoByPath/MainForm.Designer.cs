06df08dd3689f4eec37b85a7f211c9cc1d8cc0e4
Vikram Singh Saini <sspl.vssaini@gmail.com>
2021-03-03 07:11:38 +0530

    Add project files.

---
+++ UserPhotoByPath/MainForm.Designer.cs
@@ -0,0 +1,149 @@
+﻿using System.ComponentModel;
+using System.Windows.Forms;
+
+namespace UserPhotoByPath
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtLdapPath = new System.Windows.Forms.TextBox();
+            this.picUser = new System.Windows.Forms.PictureBox();
+            this.lblUser = new System.Windows.Forms.Label();
+            this.btnRetrieveDetail = new System.Windows.Forms.Button();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.lblStatus = new System.Windows.Forms.ToolStripStatusLabel();
+            this.bgwPhoto = new System.ComponentModel.BackgroundWorker();
+            ((System.ComponentModel.ISupportInitialize)(this.picUser)).BeginInit();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(66, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "LDAP Path:";
+            // 
+            // txtLdapPath
+            // 
+            this.txtLdapPath.Location = new System.Drawing.Point(94, 9);
+            this.txtLdapPath.Name = "txtLdapPath";
+            this.txtLdapPath.Size = new System.Drawing.Size(384, 23);
+            this.txtLdapPath.TabIndex = 1;
+            // 
+            // picUser
+            // 
+            this.picUser.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.picUser.Location = new System.Drawing.Point(12, 64);
+            this.picUser.Name = "picUser";
+            this.picUser.Size = new System.Drawing.Size(295, 241);
+            this.picUser.TabIndex = 2;
+            this.picUser.TabStop = false;
+            // 
+            // lblUser
+            // 
+            this.lblUser.AutoSize = true;
+            this.lblUser.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblUser.Location = new System.Drawing.Point(12, 46);
+            this.lblUser.Name = "lblUser";
+            this.lblUser.Size = new System.Drawing.Size(63, 15);
+            this.lblUser.TabIndex = 3;
+            this.lblUser.Text = "User Photo";
+            // 
+            // btnRetrieveDetail
+            // 
+            this.btnRetrieveDetail.Location = new System.Drawing.Point(403, 38);
+            this.btnRetrieveDetail.Name = "btnRetrieveDetail";
+            this.btnRetrieveDetail.Size = new System.Drawing.Size(75, 23);
+            this.btnRetrieveDetail.TabIndex = 4;
+            this.btnRetrieveDetail.Text = "Retrieve...";
+            this.btnRetrieveDetail.UseVisualStyleBackColor = true;
+            this.btnRetrieveDetail.Click += new System.EventHandler(this.btnRetrieveDetail_Click);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.lblStatus});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 321);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(488, 22);
+            this.statusStrip1.SizingGrip = false;
+            this.statusStrip1.TabIndex = 5;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(26, 17);
+            this.lblStatus.Text = "Idle";
+            // 
+            // bgwPhoto
+            // 
+            this.bgwPhoto.DoWork += new System.ComponentModel.DoWorkEventHandler(this.bgwPhoto_DoWork);
+            this.bgwPhoto.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.bgwPhoto_RunWorkerCompleted);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(488, 343);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.btnRetrieveDetail);
+            this.Controls.Add(this.lblUser);
+            this.Controls.Add(this.picUser);
+            this.Controls.Add(this.txtLdapPath);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "AD User Photo - By path";
+            ((System.ComponentModel.ISupportInitialize)(this.picUser)).EndInit();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Label label1;
+        private TextBox txtLdapPath;
+        private PictureBox picUser;
+        private Label lblUser;
+        private Button btnRetrieveDetail;
+        private StatusStrip statusStrip1;
+        private ToolStripStatusLabel lblStatus;
+        private BackgroundWorker bgwPhoto;
+    }
+}
+
---
+++ UserPhotoByPath/MainForm.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.ComponentModel;
+using System.Configuration;
+using System.DirectoryServices;
+using System.Drawing;
+using System.IO;
+using System.Windows.Forms;
+using UserPhotoByPath.Properties;
+
+namespace UserPhotoByPath
+{
+    public partial class MainForm : Form
+    {
+        /// <summary>
+        /// The LDAP provider string used to build the LDAP path
+        /// </summary>
+        public const string LDAPProvider = "LDAP://";
+
+        private static string _domainName;
+        private static string _userName;
+        private static string _password;
+        byte[] _photo;
+
+        public MainForm()
+        {
+            InitializeComponent();
+
+            // Retrieve values from app.config
+            _domainName = ConfigurationManager.AppSettings["DomainName"];
+            _userName = ConfigurationManager.AppSettings["Username"];
+            _password = ConfigurationManager.AppSettings["Password"];
+        }
+
+        private void btnRetrieveDetail_Click(object sender, EventArgs e)
+        {
+            txtLdapPath.Enabled = picUser.Enabled = btnRetrieveDetail.Enabled = false;
+            lblStatus.Text = "Please wait! Retrieving photo as per path...";
+            bgwPhoto.RunWorkerAsync(txtLdapPath.Text.Trim());
+        }
+
+        private void bgwPhoto_DoWork(object sender, DoWorkEventArgs e)
+        {
+            var ldapPath = (string) e.Argument;
+            _photo = GetUserPhotoByPath(ldapPath);
+        }
+
+        private void bgwPhoto_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            txtLdapPath.Enabled = picUser.Enabled = btnRetrieveDetail.Enabled = true;
+
+            if (e.Error == null)
+            {
+                if (_photo != null)
+                {
+                    var ms = new MemoryStream(_photo);
+                    picUser.Image = Image.FromStream(ms);
+                    lblStatus.Text = "Photo retrieved from AD successfully!";
+                }
+                else
+                {
+                    lblStatus.Text = "Photo not found in AD.";
+                    picUser.Image = Resources.no_photo;
+                }
+            }
+            else
+            {
+                lblStatus.Text = "Error occured";
+                MessageBox.Show(e.Error.ToString());
+            }
+        }
+
+        #region Helpers
+        
+        /// <summary>
+        /// Returns the user photo from AD
+        /// </summary>
+        /// <param name="sAMAccountName">The user sAMAccountName</param>
+        /// <param name="imagePropertyName">The name of the property that hold the image</param>
+        /// <returns>The photo bytes</returns>
+        public static byte[] GetUserPhoto(string sAMAccountName, string imagePropertyName = "thumbnailPhoto")
+        {
+            // Init return value
+            byte[] userPhoto = null;
+
+            using (
+                var entry = new DirectoryEntry(_domainName, _userName, _password,
+                    AuthenticationTypes.FastBind | AuthenticationTypes.Secure))
+            {
+                // Search for username
+                using (DirectorySearcher searcher = new DirectorySearcher(entry))
+                {
+                    // Filter results by SAMAccountName
+                    searcher.Filter = string.Format("(SAMAccountName={0})", sAMAccountName);
+                    searcher.PropertiesToLoad.Add(imagePropertyName);
+
+                    // Search and return only one result
+                    SearchResult result = searcher.FindOne();
+
+                    // Check if the user exists
+                    if (result == null)
+                        throw new Exception(string.Format("User \"{0}\" not found", sAMAccountName));
+
+                    // Check if the property exists and set it
+                    if (result.Properties[imagePropertyName].Count != 0)
+                        userPhoto = (byte[]) result.Properties[imagePropertyName][0];
+                }
+            }
+
+            // Return user photo
+            return userPhoto;
+        }
+
+        /// <summary>
+        /// Returns the user photo from AD given the user LDAP path
+        /// </summary>
+        /// <param name="ldapPath">The user LDAP path</param>
+        /// <param name="imagePropertyName">The name of the property that hold the image</param>
+        /// <returns>The photo bytes</returns>
+        public static byte[] GetUserPhotoByPath(string ldapPath, string imagePropertyName = "thumbnailPhoto")
+        {
+            // Init return value
+            byte[] userPhoto = null;
+            using (
+                var entry = new DirectoryEntry(ldapPath, _userName, _password,
+                    AuthenticationTypes.FastBind | AuthenticationTypes.Secure))
+            {
+                // Check if the property exists and set it
+                if (entry.Properties[imagePropertyName].Count != 0)
+                    userPhoto = (byte[]) entry.Properties[imagePropertyName][0];
+            }
+
+            // Return user photo
+            return userPhoto;
+        }
+
+        #endregion
+       
+    }
+}
